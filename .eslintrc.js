ccbeea6f7064e3a684621bd5196cf52eb11aee74
marcinkornek <marcinkornek@gmail.com>
2017-04-30 16:11:43 +0200

    Merge branch 'master' to branch 'add-react-native-config'

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  plugins: [
+    'react',
+    'react-native',
+    'jsx-a11y',
+    'import'
+  ],
+  settings: {
+    'import/resolver': 'reactnative',
+  },
+  env: {
+    jest: true
+  },
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js'] }],
+    'react/require-default-props': 0,
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'import/prefer-default-export': 0,
+    'no-unused-vars': [2, { 'argsIgnorePattern': '^_' }],
+    'max-len': [2, 100],
+    'global-require': 0,
+  },
+  globals: {
+    fetch: false,
+  },
+};
