0ebf120a23c5a7e804db815ddb1a95bf24e00f5d
Mateusz Palyz <mateusz.palyz@netguru.pl>
2017-05-04 15:01:47 +0200

    Mock Linking in jest

--- jest/setup.js
+++ jest/setup.js
@@ -7,3 +7,13 @@ jest.mock('react-native-fabric', () => ({
     logContentView: () => {},
   },
 }));
+
+// https://github.com/facebook/jest/issues/2208#issuecomment-265132583
+// https://github.com/react-community/react-navigation/issues/256#issuecomment-284173395
+jest.mock('Linking', () => ({
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  openURL: jest.fn(),
+  canOpenURL: jest.fn(),
+  getInitialURL: jest.fn(),
+}));
