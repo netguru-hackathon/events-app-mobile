479e84752163f543432c7d3e6590d0a7a08cdfec
marcinkornek <marcinkornek@gmail.com>
2017-05-09 17:14:29 +0200

    Remove not used import

--- app/containers/__tests__/SlackAuth.js
+++ app/containers/__tests__/SlackAuth.js
@@ -2,7 +2,6 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import SlackAuth from '../SlackAuth';
-import store from '../../store';
 
 // https://github.com/facebook/react-native/issues/12440
 jest.unmock('ScrollView');
