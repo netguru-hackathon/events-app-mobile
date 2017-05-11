cf77acdf37523f81417483f82a8acc09265118a3
Mateusz Palyz <mateusz.palyz@netguru.pl>
2017-05-11 16:40:27 +0200

    Add translations file and check if it works

---
+++ app/utils/translations.js
@@ -0,0 +1,14 @@
+import I18n from 'react-native-i18n';
+
+I18n.fallbacks = true;
+
+I18n.translations = {
+  en: {
+    welcome: 'Welcome',
+  },
+  pl: {
+    welcome: 'Witaj',
+  },
+};
+
+export default I18n;
