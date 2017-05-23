ab6d8166f0ffc786fd914fd6672cd43ce42162f1
marcinkornek <marcinkornek@gmail.com>
2017-05-23 22:19:34 +0200

    Add authorization header to requests, add deserializer opts

--- app/api/client.js
+++ app/api/client.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import update from 'immutability-helper';
 import { baseURL } from '../constants/constants';
 
 const JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
@@ -8,10 +9,14 @@ export const client = axios.create({
   responseType: 'json',
 });
 
+const opts = {
+  keyForAttribute: 'camelCase',
+};
+
 export const clientOptions = {
   returnRejectedPromiseOnError: true,
   onSuccess: ({ action, next, response }) => {
-    new JSONAPIDeserializer().deserialize(response.data)
+    new JSONAPIDeserializer(opts).deserialize(response.data)
       .then((data) => {
         const nextAction = {
           type: action.types[1],
@@ -23,5 +28,20 @@ export const clientOptions = {
         next(nextAction);
         return nextAction;
       });
+  },
+  interceptors: {
+    request: [
+      // eslint-disable-next-line
+      function ({ getState, dispatch, getSourceAction }, req) {
+        if (!getState().auth.item.token) return req;
+        return update(req, {
+          $merge: {
+            headers: {
+              authorization: `Bearer ${getState().auth.item.token}`,
+            },
+          },
+        });
+      },
+    ],
   },
 };
