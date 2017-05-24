89272ddd76fe058c9b29c2c457c2d1d84618a5b1
marcinkornek <marcinkornek@gmail.com>
2017-05-24 21:23:51 +0200

    Add user info to settings

--- app/containers/screens/Settings.js
+++ app/containers/screens/Settings.js
@@ -11,23 +11,41 @@ import {
 } from 'react-native';
 import I18n from '../../utils/translations';
 import colors from '../../constants/colors';
+import { RenderActivityIndicator } from '../shared/RenderActivityIndicator';
 
 // actions
 import { logoutUser } from '../../actions/auth';
 
 const styles = StyleSheet.create({
+  email: {
+    marginBottom: 10,
+  },
   icon: {
     width: 20,
     height: 20,
   },
+  image: {
+    width: 120,
+    height: 120,
+    margin: 5,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
   view: {
     margin: 10,
+    alignItems: 'center',
   },
 });
 
 class Settings extends Component {
   static propTypes = {
-    token: PropTypes.string.isRequired,
+    user: PropTypes.shape({
+      email: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      userUrl: PropTypes.string.isRequired,
+    }),
     logoutUser: PropTypes.func.isRequired,
   };
 
@@ -55,23 +73,27 @@ class Settings extends Component {
   }
 
   render() {
-    const { token } = this.props;
+    const { user } = this.props;
+
+    if (!user) return <RenderActivityIndicator />;
 
     return (
       <View style={styles.view}>
+        <Image source={{ uri: user.avatarUrl }} style={styles.image} />
+        <Text style={styles.name}>{user.name}</Text>
+        <Text style={styles.email}>{user.email}</Text>
         <Button
           onPress={this.logout.bind(this)}
           title={I18n.t('logout')}
           color="#841584"
         />
-        <Text>{token}</Text>
       </View>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  token: state.auth.item.token,
+  user: state.auth.item.user,
 });
 
 const mapDispatchToProps = {
